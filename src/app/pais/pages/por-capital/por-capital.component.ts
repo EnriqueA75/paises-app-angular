3bbd096851ef31fb23cde2829ef0bdd478da31e7
EnriqueA75 <Enrique_ceas@hotmail.com>
2022-02-08 14:54:22 -0600

    search countries by capital

--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-capital',
@@ -6,11 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PorCapitalComponent implements OnInit {
+export class PorCapitalComponent {
 
-  constructor() { }
+  constructor(private paisService: PaisService) { }
 
-  ngOnInit(): void {
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = []
+  
+  buscar( termino: string ){
+    console.log(this.termino);
+    this.termino = termino;
+    
+    this.paisService.buscarPorCapital(this.termino)
+      .subscribe( (paises) => {
+        this.paises = paises;
+        this.hayError = false;
+        console.log(this.paises)
+      }, (error) => {
+        this.hayError = true;
+        this.paises = [];
+      })
+  }
+  sugerencias( termino: string ){
+    this.hayError = false;
+    //TODO: crear sugerencias
   }
 
 }
