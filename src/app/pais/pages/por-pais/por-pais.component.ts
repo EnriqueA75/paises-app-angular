9435c5d81ced42d8f827ca5c696957c1baedb186
EnriqueA75 <Enrique_ceas@hotmail.com>
2022-02-09 11:50:53 -0600

    country suggestions and finished project

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -5,14 +5,20 @@ import { Country } from '../../interfaces/pais.interface';
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styles: [
+  styles: [ `
+    li {
+      cursor: pointer;
+    }
+  `
   ]
 })
 export class PorPaisComponent {
 
   termino: string = '';
   hayError: boolean = false;
-  paises: Country[] = []
+  paises: Country[] = [];
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias: boolean = false;
   
   private apiUrl: string = 'https://restcountries.com/v2';
 
@@ -21,7 +27,7 @@ export class PorPaisComponent {
   buscar( termino: string ){
     console.log(this.termino);
     this.termino = termino;
-    
+    this.mostrarSugerencias = false;
     this.paisService.buscarPais(this.termino)
       .subscribe( (paises) => {
         this.paises = paises;
@@ -34,6 +40,17 @@ export class PorPaisComponent {
   }
   sugerencias( termino: string ){
     this.hayError = false;
-    //TODO: crear sugerencias
+    this.termino = termino;
+    this.mostrarSugerencias = true;
+    this.paisService.buscarPais(termino)
+      .subscribe(paises => {
+        this.paisesSugeridos = paises.splice(0,5);
+      }, (error => {
+        this.paisesSugeridos = [];
+      }))
   }
+  buscarSugerido(termino: string) {
+    this.buscar(termino);
+  }
+
 }
